7a6302605b006ba53ec6093b43411cb97ab4bf50
Bishal Neupane <Neupanebishal07@gmail.com>
2023-07-17 18:44:46 +0545

    Setup pwa

--- next.config.js
+++ next.config.js
@@ -1,4 +1,8 @@
+const withPWA = require("@imbios/next-pwa")({
+  dest: "public",
+});
+
 /** @type {import('next').NextConfig} */
 const nextConfig = {};
 
-module.exports = nextConfig;
+module.exports = withPWA(nextConfig);
